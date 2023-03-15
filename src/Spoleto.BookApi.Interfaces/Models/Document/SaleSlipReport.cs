5294ff2ef09f1489ed7a55c436a855fe8282487f
Oleg Nadymov <OlegNadymov@gmail.com>
2023-03-15 13:13:09 +0300

    SaleSlipReport

---
+++ src/Spoleto.BookApi.Interfaces/Models/Document/SaleSlipReport.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Spoleto.BookApi.Interfaces.Models
+{
+    /// <summary>
+    /// Отчёт о розничных продажах (ОРП)
+    /// </summary>
+    public class SaleSlipReport : PersistentObjectBase
+    {
+        /// <summary>
+        /// Коллекция чеков для формирования (ОРП)
+        /// </summary>
+        public List<SaleSlip> Items { get; set; }
+    }
+}
