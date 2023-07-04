ed637a7882a49c46a6c54ab242f16d2f99306eb8
o.voronina <o.voronina@cashmere.ru>
2023-07-04 12:28:55 +0300

    План счетов

---
+++ src/Spoleto.BookApi.Interfaces/Interfaces/Reference/IPlanAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spoleto.BookApi.Interfaces
+{
+    /// <summary>
+    /// План счетов
+    /// </summary>
+    public interface IPlanAccount : IPersistentReference
+    {
+        /// <summary>
+        /// Код
+        /// </summary>
+       string Code { get; set; }
+
+       /// <summary>
+       /// Наименование
+       /// </summary>
+       string Description { get; set; }
+    }
+}
---
+++ src/Spoleto.BookApi.Interfaces/Models/Reference/PlanAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Spoleto.BookApi.Interfaces.Models
+{
+    /// <summary>
+    /// План счетов
+    /// </summary>
+    public class PlanAccount : PersistentObjectBase, IPlanAccount
+    {
+        /// <summary>
+        /// Код
+        /// </summary>
+        public string Code { get; set; }
+
+        /// <summary>
+        /// Наименование
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
