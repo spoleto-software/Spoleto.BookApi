87ed86b50ea712d3f87c41ef92b9c975ccd156fe
o.nadymov <o.nadymov@spoleto.ru>
2024-02-23 15:35:09 +0300

    Login/Refresh requests

---
+++ src/Spoleto.BookApi.Interfaces/Models/Auth/LoginRequest.cs
@@ -0,0 +1,18 @@
+﻿namespace Spoleto.BookApi.Interfaces.Models.Auth
+{
+    /// <summary>
+    /// The login request.
+    /// </summary>
+    public class LoginRequest
+    {
+        /// <summary>
+        /// Gets or sets whether the email or the phone.
+        /// </summary>
+        public string EmailOrPhone { get; set; }
+
+        /// <summary>
+        /// Gets or sets the password.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
---
+++ src/Spoleto.BookApi.Interfaces/Models/Auth/RefreshRequest.cs
@@ -0,0 +1,13 @@
+﻿namespace Spoleto.BookApi.Interfaces.Models.Auth
+{
+    /// <summary>
+    /// The request for token refresh.
+    /// </summary>
+    public class RefreshRequest
+    {
+        /// <summary>
+        /// Gests or sets the refresh token.
+        /// </summary>
+        public string RefreshToken { get; set; }
+    }
+}
