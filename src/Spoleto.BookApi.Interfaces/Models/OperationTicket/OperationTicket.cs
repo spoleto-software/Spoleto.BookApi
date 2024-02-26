dc3424b7654f2ac6149f3ad9a3ebf4b882939bba
o.nadymov <o.nadymov@spoleto.ru>
2024-02-26 19:23:48 +0300

    OperationTicket

--- src/Spoleto.BookApi.Interfaces/Models/OperationTicket/OperationTicket.cs
+++ src/Spoleto.BookApi.Interfaces/Models/OperationTicket/OperationTicket.cs
@@ -5,14 +5,12 @@ namespace Spoleto.BookApi.Interfaces.Models.OperationTicket
     /// <summary>
     /// Тикеты на обработку контейнеров объектов.
     /// </summary>
-    public class OperationTicket
+    public class OperationTicket : PersistentObjectBase
     {
         /// <summary>
         /// A ticket that is not found.
         /// </summary>
         public static readonly OperationTicket NotFound = new OperationTicket { Status = OperationTicketStatus.NoFound };
-
-        public Guid Id { get; set; }
 
         public OperationTicketStatus Status { get; set; }
 
