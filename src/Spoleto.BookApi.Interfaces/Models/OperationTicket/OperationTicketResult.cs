32e8ae474c81b121f53dff0e67a522d40e31dd21
o.nadymov <o.nadymov@spoleto.ru>
2024-03-07 16:00:47 +0300

    OperationTicketResult

---
+++ src/Spoleto.BookApi.Interfaces/Models/OperationTicket/OperationTicketResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Spoleto.BookApi.Interfaces.Models.OperationTicket
+{
+    /// <summary>
+    /// The operation ticket result.
+    /// </summary>
+    public class OperationTicketResult
+    {
+        /// <summary>
+        /// The operation ticket identity.
+        /// </summary>
+        public Guid Id { get; set; }
+    }
+}
