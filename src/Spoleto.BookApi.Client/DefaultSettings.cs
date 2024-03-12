d1e7091d82dad50cf4937aad45d0f9381fe2ceeb
o.nadymov <o.nadymov@spoleto.ru>
2024-03-12 16:24:02 +0300

    misc

--- src/Spoleto.BookApi.Client/DefaultSettings.cs
+++ src/Spoleto.BookApi.Client/DefaultSettings.cs
@@ -5,7 +5,9 @@ namespace Spoleto.BookApi.Client
     public static class DefaultSettings
     {
         public const string ContentType = Spoleto.Common.ContentTypes.ApplicationJson;
+
         public const string Charset = "utf-8";
+
         public static readonly Encoding Encoding = Encoding.GetEncoding(Charset);
     }
 }
